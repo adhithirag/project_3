63ff10f6fb3075f3d588fc18d938e6cdd67373c3
Marisol C <147443547+MarisolCor@users.noreply.github.com>
2024-02-12 18:53:52 -0800

    add new map changes

--- firemap/logic.js
+++ firemap/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint as queryUrl.
-let queryUrl = "../California_Fire_Perimeters_(all).geojson";
+let queryUrl = "http://127.0.0.1:5000/api/v1.0/find_features";
 let myMap = L.map("map", {
   center: [37.807246697771554, -122.43170695660642],
   zoom: 6
@@ -10,10 +10,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  L.geoJson(data,{
-    style: function(feature) {
+
+addDropDown();
+
+function addDropDown() {
+  /////// creates dropdown
+  let customControl = L.control({position: 'topleft'});
+  customControl.onAdd = function (map) {
+    let div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = '<select class="wide-control"><option value="2010">2010</option><option value="2020">2020</option><option value="2000">2000</option></select>';
+    div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+    div.firstChild.onchange = function () {
+      requestMarkers(this.value);
+    };
+    return div;
+  };
+
+  customControl.addTo(myMap); // Add the custom control to your map
+}
+
+
+//requestMarkers(2020);
+
+function renderMarkers(data) {
+  L.clearLayers();
+  L.geoJson(data, {
+    style: function (feature) {
       return {
         color: "red",
         // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
@@ -22,4 +44,13 @@ d3.json(queryUrl).then(function (data) {
       };
     }
   }).addTo(myMap);
-});
+}
+
+// Perform a GET request to the query URL/
+function requestMarkers(year){
+  d3.json(`${queryUrl}/${year}`).then(function (data) {
+    renderMarkers(data);
+  });
+
+}
+
