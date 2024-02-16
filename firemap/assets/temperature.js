51b3c64e10792ba02d5fe018cd8043e992a4d96d
Marisol C <147443547+MarisolCor@users.noreply.github.com>
2024-02-15 19:26:56 -0800

    add slider

---
+++ firemap/assets/temperature.js
@@ -0,0 +1,13 @@
+let temperatureURL = "https://raw.githubusercontent.com/adhithirag/project_3/temp_mapping/Nasa%20Data/23%20years/averaged_by_years/averaged_tempanomaly_";
+
+function initTemperatureLogic(){
+
+}
+
+
+function renderTemperatureMarkers(data, year){}
+
+function requestTemperatureMarkers(year){
+
+}
+
