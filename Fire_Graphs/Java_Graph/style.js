a8ac93cda5d4b12880ff34423b5feb0dc5d0c2eb
Alex Lazzari <alexandre.lazzari1@gmail.com>
2024-02-19 21:16:38 -0800

    updated graph

---
+++ Fire_Graphs/Java_Graph/style.js
@@ -0,0 +1,61 @@
+{
+    "title": {
+        "font": {
+            "family": "Arial",
+            "size": 28,
+            "color": "#333"
+        },
+        "x": 0.5,
+        "y": 0.9
+    },
+    "xaxis": {
+        "title": {
+            "text": "Fire Name",
+            "font": {
+                "family": "Arial",
+                "size": 24,
+                "color": "#555"
+            }
+        },
+        "automargin": true,
+        "tickfont": {
+            "family": "Arial",
+            "size": 18,
+            "color": "#777"
+        },
+        "showgrid": true,
+        "gridcolor": "#ddd"
+    },
+    "yaxis": {
+        "title": {
+            "text": "Acreage Burned",
+            "font": {
+                "family": "Arial",
+                "size": 24,
+                "color": "#555"
+            }
+        },
+        "tickfont": {
+            "family": "Arial",
+            "size": 18,
+            "color": "#777"
+        },
+        "showgrid": true,
+        "gridcolor": "#ddd"
+    },
+    "legend": {
+        "font": {
+            "family": "Arial",
+            "size": 20,
+            "color": "#444"
+        }
+    },
+    "margin": {
+        "l": 120,
+        "r": 120,
+        "b": 120,
+        "t": 140,
+        "pad": 8
+    },
+    "plot_bgcolor": "#f7f7f7"
+}
